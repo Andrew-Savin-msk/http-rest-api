803475bb6713753c4b2149105377eb5cb2bf01c5
Andrew-Savin-msk <4andreysavin@gmail.com>
2024-01-04 16:16:31 +0300

    added router and handler, addet test

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,6 +1,10 @@
 package apiserver
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
@@ -21,23 +25,37 @@ func New(config *Config) *APIServer {
 }
 
 func (s *APIServer) Start() error {
+	const op = "internal.app.apiserver.Start"
 	err := s.configureLogger()
 	if err != nil {
-		return err
+		return fmt.Errorf("error in: %s with starting server, ended with error: %w", op, err)
 	}
+
+	s.configureRouter()
 
 	s.logger.Info("starting api server")
 
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
+	const op = "internal.app.apiserver.configureLogger"
 	level, err := logrus.ParseLevel(s.config.LogLevel)
 	if err != nil {
-		return err
+		return fmt.Errorf("error in: %s with setting configure level, ended with error: %w", op, err)
 	}
 
 	s.logger.SetLevel(level)
 
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+	}
 }
