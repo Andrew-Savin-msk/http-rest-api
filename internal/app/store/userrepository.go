a290708e1b51bcfc51683456ed62465a42a88867
Andrew-Savin-msk <4andreysavin@gmail.com>
2024-01-17 13:02:07 +0300

    siccesfuly conneted database migrations

---
+++ internal/app/store/userrepository.go
@@ -0,0 +1,13 @@
+package store
+
+type UserRepository struct {
+	store *Store
+}
+
+// func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+// 	return nil, nil
+// }
+
+// func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+// 	return nil, nil
+// }
